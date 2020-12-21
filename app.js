e2dc659a5858f144cffc60acd7ab8dfccc88b5c9
cindy803 <sheeli.803@gmail.com>
2020-12-21 01:59:02 -0800

    initial commit

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
-// mongodb+srv://admin:<password>@mern.uywem.mongodb.net/<dbname>?retryWrites=true&w=majority
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-}); 
+mongoose
+  .connect(db, { 
+      useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World!!"));
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
-
-app.listen(port, () => {console.log(`Listening on Port ${port}`)});
+app.listen(port, () => console.log(`Server is running on port ${port}`));
