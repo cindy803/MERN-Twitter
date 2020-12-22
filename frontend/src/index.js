3f6cb847992ea25d73865ae806f3d1cf17ddec9f
cindy803 <sheeli.803@gmail.com>
2020-12-21 17:39:30 -0800

    frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 
 // We will create this component shortly
 import Root from './components/root';
